884b360f8f4b407e81c682365458478652cb3bd9
zhiqilin <zhiqilinn@gmail.com>
2022-06-22 16:45:20 -0400

    login & signup & navigation done

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -40,6 +40,29 @@ export const restoreUser = () => async dispatch => {
     return response;
   };
 
+
+  export const signup = (user) => async (dispatch) => {
+    const { username, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
+  export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+      method: 'DELETE',
+    });
+    dispatch(removeUser());
+    return response;
+  };
 //-----------------------REDUCER--------------------------
 const initialState = { user: null };
 
