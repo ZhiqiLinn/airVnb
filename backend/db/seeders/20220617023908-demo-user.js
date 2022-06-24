ea758957f34549c8e791a32a814767b57d59233a
zhiqilin <zhiqilinn@gmail.com>
2022-06-23 20:03:12 -0400

    database setup done

--- backend/db/seeders/20220617023908-demo-user.js
+++ backend/db/seeders/20220617023908-demo-user.js
@@ -6,17 +6,20 @@ module.exports = {
     return queryInterface.bulkInsert('Users', [
       {
         email: 'demo@user.io',
-        username: 'Demo-lition',
+        username: 'Demo',
+        profilePic: null,
         hashedPassword: bcrypt.hashSync('password')
       },
       {
         email: 'user1@user.io',
         username: 'FakeUser1',
+        profilePic: null,
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
         email: 'user2@user.io',
         username: 'FakeUser2',
+        profilePic: null,
         hashedPassword: bcrypt.hashSync('password3')
       }
     ], {});
