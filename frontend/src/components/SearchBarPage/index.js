4bc00b46c35b1dc0f6b69eb6f0a89b30cb5c2e7e
zhiqilin <zhiqilinn@gmail.com>
2022-07-03 14:05:25 -0400

    added search bar css

--- frontend/src/components/SearchBarPage/index.js
+++ frontend/src/components/SearchBarPage/index.js
@@ -3,6 +3,8 @@ import { useDispatch } from 'react-redux';
 import { getAllListings } from '../../store/listing';
 import { Link, NavLink } from 'react-router-dom';
 import "./SearchBar.css"
+import searchIcon from '../../images/search.png';
+import closeIcon from '../../images/close.png';
 
 const SearchBarPage = ({allListings}) => {
     const dispatch = useDispatch();
@@ -10,36 +12,53 @@ const SearchBarPage = ({allListings}) => {
 
     const [filteredData, setFilteredData] = useState([]);
     
-    //-----------FILL THE LISITNGSDATA STATE-------------------
-    useEffect(() => {
-        dispatch(getAllListings())
-    }, [dispatch]);
-
     //------------FILTER KEYWORDS FROM ALL LISTING------------
+    let newFilter;
     const handleFilter = (e) => {
         const searchWord = e.target.value
-        const newFilter = allListingsArr.filter(listing => {
+        if(searchWord.length === 0) newFilter=[];
+        newFilter = allListingsArr.filter(listing => {
             return listing.name.toLowerCase().includes(searchWord.toLowerCase())
         })
         setFilteredData(newFilter)
     }
+
     //------------CLEAR INPUT -------------------------------
     const handleClear = (e) => {
-        e.target.value = "";
+        setFilteredData([])
     }
+
+    //-----------FILL THE LISITNGSDATA STATE-------------------
+    useEffect(() => {
+        dispatch(getAllListings())
+    }, [dispatch, filteredData]);
+
     return(
         <div className="search">
-            <div className="search-input">
+            <div className="search-bar-div">
+                <label>
+                    <h1>Search Your Listing Today!</h1>
+                    <br></br>
                 <input 
+                    className="search-input"
                     type="text"
                     placeholder='Enter a Listing Title...'
                     onChange={handleFilter} 
                  />
+
+                </label>
+                <div className='glass'>
+                    
                     {filteredData.length === 0 ? 
-                        <i className="fa-solid fa-magnifying-glass" ></i> 
+                        <div>
+                            <img height="30px" width="30px" src={searchIcon} alt='search' ></img>   
+                        </div>
                         :
-                        <i className="fa-solid fa-xmark" onClick={handleClear}></i>
+                        <div>
+                            <img height="30px" width="30px" src={closeIcon} alt='search' onClick={handleClear} ></img>   
+                        </div>
                      }
+                </div>
                     
 
             </div>
